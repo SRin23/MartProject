1523d07c6bc35ab9ae2198be2b5dcb78f53f659b
SRin23 <serin4723@naver.com>
2022-02-07 14:36:55 +0900

    [delete] 미사용 파일 삭제 - #2

--- MartProject/login.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "main.h"
-#include "Manager.h"
-#include "Cashier.h"
-#include "util.h"
-
-void login();
--- MartProject/util.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include "main.h"
-
-void init();
-void gotoxy(int, int);
-int keyControl();
